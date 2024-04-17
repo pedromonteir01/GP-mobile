eeacf4651a4970bc3224f29e90f2a1bae0345c9d
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-04-17 14:31:22 -0300

    feat: page companys

---
+++ src/screens/Companys/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import listCompanys from "../../models/companys";
+
+export default function Companys() {
+    return(
+        <View>
+
+        </View>
+    );
+}

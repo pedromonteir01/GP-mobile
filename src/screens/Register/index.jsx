0c33759bf4947465baa29dea9469610440467369
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-04-24 14:22:54 -0300

    feat: data inputs and prop route

--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -1,10 +1,22 @@
 import { View } from "react-native";
 import styles from "./styles";
+import { useState } from "react";
 
-export default function Register() {
+export default function Register({ route }) {
+
+    let { companys, edit } = route.params;
+
+    const [isUpdate, setIsUpdate] = useState(edit);
+
+    //data inputs
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [cnpj, setCnpj] = useState('');
+    const [telephone, setTelephone] = useState('');
+
     return(
         <View style={styles.container}>
-
+            
         </View>
     );
 }

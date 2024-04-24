d6fdd781e973ec2d9fd39e926b0c0b4faa33c897
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-04-24 15:03:26 -0300

    feat: logic register

--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -1,10 +1,15 @@
-import { View } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import styles from "./styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import listCompanys from "../../models/companys";
+import Company from "../../models/company";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Register({ route }) {
 
-    let { companys, edit } = route.params;
+    let { company, edit } = route.params;
+
+    const navigation = useNavigation();
 
     const [isUpdate, setIsUpdate] = useState(edit);
 
@@ -13,10 +18,50 @@ export default function Register({ route }) {
     const [email, setEmail] = useState('');
     const [cnpj, setCnpj] = useState('');
     const [telephone, setTelephone] = useState('');
+    const [contact, setContact] = useState(false);
+
+    useEffect(() => {
+        if(isUpdate) {
+            setName(company.name);
+            setEmail(company.email);
+            setCnpj(String(company.cnpj));
+            setTelephone(String(company.telephone));
+            setContact(Boolean(company.contact));
+            //editar
+            setIsUpdate(true);
+        } else {
+            clearInputs();
+        }
+    }, [company, edit]);
+
+    const clearInputs = () => {
+        setIsUpdate(false);
+        edit = false;
+        setName('');
+        setEmail('');
+        setCnpj('');
+        setTelephone('');
+        setContact('');
+    }
+
+    const handleCompany = () => {
+        if(isUpdate) {
+            listCompanys.putCompany(company.id, company.name, company.email, company.cnpj, company.telephone, company.contact);
+            clearInputs();
+        } else {
+            const company = new Company(name, email, cnpj, telephone, contact);
+            listCompanys.postCompany(company);
+            clearInputs();
+        }
+        navigation.navigate('Companys');
+    }
 
     return(
         <View style={styles.container}>
-            
+            <Text>Registre-se</Text>
+            <View style={styles.forms}>
+
+            </View>
         </View>
     );
 }

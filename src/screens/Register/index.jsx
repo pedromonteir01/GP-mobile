88e9dd657473d24462c9961e99c9f33d3791bcb4
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-04-24 14:13:16 -0300

    feat: register structure

---
+++ src/screens/Register/index.jsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import styles from "./styles";
+
+export default function Register() {
+    return(
+        <View style={styles.container}>
+
+        </View>
+    );
+}

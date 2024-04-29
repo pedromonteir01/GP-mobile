fb7598408aa079ba9b1918a7251d4c6129057c30
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-04-29 08:16:28 -0300

    feat: logic get all companys

--- src/screens/Companys/index.jsx
+++ src/screens/Companys/index.jsx
@@ -1,9 +1,24 @@
 import { View, Text } from "react-native";
 import listCompanys from "../../models/companys";
+import styles from "./styles";
+import { useIsFocused, useNavigation } from "@react-navigation/native";
+import { useEffect, useState } from "react";
 
 export default function Companys() {
+
+    const navigation = useNavigation();
+    const [ companys, setCompanys ] = useState([]);
+    const isFocused = useIsFocused();
+
+    useEffect(() => {
+         if(isFocused) {
+            const list = listCompanys.getAllCompanys();
+            setCompanys(list);
+         }
+    }, [isFocused]);
+
     return(
-        <View>
+        <View style={styles.container}>
 
         </View>
     );

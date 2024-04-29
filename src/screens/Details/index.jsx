cdcaefa24f18def1f9015f6db37583a2c86f6571
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-04-29 09:44:41 -0300

    feat: finish logic details

--- src/screens/Details/index.jsx
+++ src/screens/Details/index.jsx
@@ -1,10 +1,43 @@
-import { View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import styles from "./styles";
+import listCompanys from "../../models/companys";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Details() {
+export default function Details({ route }) {
+
+    const { data } = route.params;
+
+    const navigation = useNavigation();
+
+    const removeCompany = () => {
+        listCompanys.removeCompany(data.id);
+        navigation.navigate('Companys');
+    }
+
+    const editCompany = () => {
+        navigation.navigate('Register', { company: data, edit: true  });
+    }
+
     return (
         <View style={styles.container}>
+            {/* quando estilizar, adicionar as tags necessarias, pois abaixo so vai ter como faz pra exibir os dados*/}
+            <Text>{data.name}</Text>
+            <Text>{data.email}</Text>
+            <Text>{data.cnpj}</Text>
+            <Text>{data.telephone}</Text>
+            <Text>{ contact ? 'Sim' : 'Não'}</Text>
 
+            <TouchableOpacity onPress={editCompany}>
+                <Text>
+                    Editar
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={removeCompany}>
+                <Text>
+                    Excluir
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 }

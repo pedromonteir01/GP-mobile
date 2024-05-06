934fab861a9b30d0e76050d63c4984bcf7c0af17
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-05-06 07:36:57 -0300

    fix: details page

--- src/screens/Details/index.jsx
+++ src/screens/Details/index.jsx
@@ -18,4 +18,22 @@ export default function Details({ route }) {
         navigation.navigate('Register', { company: data, edit: true });
     }
 
+    return (
+        <View style={styles.container}>
+            {/* quando estilizar, adicionar as tags necessarias, pois abaixo so vai ter como faz pra exibir os dados*/}
+            <Text>{data.name}</Text>
+            <Text>{data.email}</Text>
+            <Text>{data.cnpj}</Text>
+            <Text>{data.telephone}</Text>
+            <Text>{data.contact ? 'Sim' : 'Não'}</Text>
+
+            <TouchableOpacity onPress={editCompany}>
+                <Text>Editar</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={removeCompany}>
+                <Text>Excluir</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }

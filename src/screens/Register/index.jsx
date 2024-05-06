a3dbea9bad6d944407de7132d248532a9ba25ed3
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-05-06 07:36:05 -0300

    fix: register page

--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -1,11 +1,11 @@
-import { View, Text, TouchableOpacity, Image, TextInput, ScrollView, ImageBackground } from "react-native"; // Importação corrigida
-
+import { View, Text, TouchableOpacity, Image, TextInput } from "react-native";
 import styles from "./styles";
 import { useEffect, useState } from "react";
 import listCompanys from "../../models/companys.js";
 import Company from "../../models/company.js";
 import { useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
+
 
 export default function Register({ route }) {
 
@@ -69,110 +69,105 @@ export default function Register({ route }) {
 
     return (
         <View style={styles.container}>
-           
-                <ImageBackground source={require("../../../assets/images/backGround.jpeg")} resizeMode="cover" style={styles.image}></ImageBackground>
-                <Image source={require("../../../assets/images/logo.png")} style={styles.logo} />
 
-
-                <Text style={styles.texto1}>Faça parte da família!</Text>
-                {/*  Formulário */}
-                <View style={styles.forms}>
-                    {/*  Campo dos inputs */}
-                    <View style={styles.inputField}>
-                        <TextInput
-                            value={name}
-                            onChangeText={setName}
-                            placeholder="Razão Social"
-                        />
-                        <Feather style={styles.icones1}
-                            name="home"
-                            size={24}
-                            color={"#000"}
-                        />
-                    </View>
-
-                    <View style={styles.inputField}>
-                        <TextInput
-                            value={email}
-                            onChangeText={setEmail}
-                            placeholder="E-mail"
-                        />
-                        <Feather style={styles.icones1}
-                            name="mail"
-                            size={24}
-                            color={"#000"}
-                        />
-                    </View>
-
-
-                    <View style={styles.inputField}>
-                        <TextInput
-                            value={cnpj}
-                            onChangeText={setCnpj}
-                            placeholder="CNPJ"
-                        />
-                        <Feather style={styles.icones1}
-                            name="map"
-                            size={24}
-                            color={"#000"}
-                        />
-                    </View>
-
-
-                    <View style={styles.inputField}>
-                        <TextInput
-                            value={telephone}
-                            onChangeText={setTelephone}
-                            placeholder="Telefone(+55)"
-                        />
-                        <Feather style={styles.icones1}
-                            name="phone"
-                            size={24}
-                            color={"#000"}
-                        />
-                    </View>
-                    {/*  Radio inputs */}
-
-                    <Text style={styles.texto2}>Gostaria que entrássemos em contato?</Text>
-                    <View style={styles.radioInputs}>
-                        <TouchableOpacity onPress={() => setSelectedRadio(1)}>
-                            <View style={styles.wrapper}>
-                                <View style={styles.radio}>
-                                    {
-                                        selectedRadio == 1 ? <View style={styles.radioBg}></View> : null
-                                    }
-                                </View>
-                                <Text style={styles.radioText}>Sim</Text>
-                            </View>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={() => setSelectedRadio(2)}>
-                            <View style={styles.wrapper}>
-                                <View style={styles.radio}>
-                                    {
-                                        selectedRadio == 2 ? <View style={styles.radioBg}></View> : null
-                                    }
-                                </View>
-                                <Text style={styles.radioText}>Não</Text>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.handle}>
-                        <View style={styles.button} >
-                            <TouchableOpacity onPress={() => handleCompany()}>
-                                <Text>{isUpdate ? 'Atualizar' : 'Cadastrar'}</Text>
-                            </TouchableOpacity>
-                        </View>
-                        {isUpdate && (
-                            <TouchableOpacity style={styles.registerBtn} onPress={clearInputs}>
-                                <Text style={{ textTransform: 'uppercase' }}>Cancelar Edição</Text>
-                            </TouchableOpacity>
-                        )}
-                    </View>
-                    
+            <Text>Faça parte da família!</Text>
+            <Text>Cadastre-se como nossa filial e creça conosco!</Text>
+            {/*  Formulário */}
+            <View style={styles.forms}>
+                {/*  Campo dos inputs */}
+                <View style={styles.inputField}>
+                    <TextInput
+                        value={name}
+                        onChangeText={setName}
+                        placeholder="Razão Social"
+                    />
+                    <Feather
+                        name="home"
+                        size={24}
+                        color={"#000"}
+                    />
                 </View>
 
-            
+                <View style={styles.inputField}>
+                    <TextInput
+                        value={email}
+                        onChangeText={setEmail}
+                        placeholder="E-mail"
+                    />
+                    <Feather
+                        name="mail"
+                        size={24}
+                        color={"#000"}
+                    />
+                </View>
+
+
+                <View style={styles.inputField}>
+                    <TextInput
+                        value={cnpj}
+                        onChangeText={setCnpj}
+                        placeholder="CNPJ"
+                    />
+                    <Feather
+                        name="map"
+                        size={24}
+                        color={"#000"}
+                    />
+                </View>
+
+
+                <View style={styles.inputField}>
+                    <TextInput
+                        value={telephone}
+                        onChangeText={setTelephone}
+                        placeholder="Telefone(+55)"
+                    />
+                    <Feather
+                        name="phone"
+                        size={24}
+                        color={"#000"}
+                    />
+                </View>
+                {/*  Radio inputs */}
+
+                <View style={styles.radioInputs}>
+                    <TouchableOpacity onPress={() => setSelectedRadio(1)}>
+                        <View style={styles.wrapper}>
+                            <View style={styles.radio}>
+                                {
+                                    selectedRadio == 1 ? <View style={styles.radioBg}></View> : null
+                                }
+                            </View>
+                            <Text style={styles.radioText}>Sim</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => setSelectedRadio(2)}>
+                        <View style={styles.wrapper}>
+                            <View style={styles.radio}>
+                                {
+                                    selectedRadio == 2 ? <View style={styles.radioBg}></View> : null
+                                }
+                            </View>
+                            <Text style={styles.radioText}>Não</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.handle}>
+                    <TouchableOpacity onPress={() => handleCompany()}>
+                        <Text>{isUpdate ? 'Atualizar' : 'Cadastrar'}</Text>
+                    </TouchableOpacity>
+                    {isUpdate && (
+                    <TouchableOpacity style={styles.registerBtn} onPress={clearInputs}>
+                        <Text style={{ textTransform: 'uppercase' }}>Cancelar Edição</Text>
+                    </TouchableOpacity>
+                )}
+                </View>
+                <Text>Indeciso? Se ainda houver dúvidas</Text>
+                <TouchableOpacity onPress={() => navigation.navigate('Aboutus')}>
+                    <Text style={{ color: 'red' }}>Conheça mais um pouco sobre nós!</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }

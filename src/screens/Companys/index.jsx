8c3a2e402a47e54e8673ad864ce775268d428d66
Pedro Monteiro <pedrohenriquesilva@aluno.senai.br>
2024-05-06 07:37:52 -0300

    fix: companys page

--- src/screens/Companys/index.jsx
+++ src/screens/Companys/index.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
-import { useIsFocused, useNavigation } from "@react-navigation/native";
+import { View, Text, ScrollView } from "react-native";
 import listCompanys from "../../models/companys";
 import styles from "./styles";
-
+import { useIsFocused, useNavigation } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import Card from "../../components/Card";
 
 export default function Companys() {
+
     const navigation = useNavigation();
     const [companys, setCompanys] = useState([]);
     const isFocused = useIsFocused();
@@ -17,42 +18,19 @@ export default function Companys() {
         }
     }, [isFocused]);
 
-    const editCompany = (companyId) => {
-        
-    };
-
-    const removeCompany = (companyId) => {
-      
-    };
-
     return (
         <View style={styles.container}>
             <ScrollView horizontal={false}>
                 <View style={styles.contentCompanys}>
-                    {companys.length > 0 ? (
-                        companys.map((company) => (
-                            <Card
-                                data={company}
-                                key={company.id}
-                                editCompany={editCompany}
-                                removeCompany={removeCompany}
-                            />
-                        ))
-                    ) : (
-                        <View>
-                            <Text> Veja nossas parceiras.</Text>
-                            <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-                                <Text >Se junto a GP!</Text>
-                            </TouchableOpacity>
-                           
-                            <Text style={styles.text}> email: {data.email} </Text>
-                        
-                           
-
-
-
-                        </View>
-                    )}
+                    {
+                        companys.length > 0 ? (
+                            companys.map((company) => (
+                                <Card data={company} key={company.id}/>
+                            ))
+                        ) : (
+                            <Text> Não há empresas registradas </Text>
+                        )
+                    }
                 </View>
             </ScrollView>
         </View>

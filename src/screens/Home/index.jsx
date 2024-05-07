e1c9acaa346676aafa4ec397ddaba1aac7bc0e5f
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2024-05-07 16:55:32 -0300

    adiciao do carousel

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,39 +1,77 @@
-import { View, Image, Text } from "react-native";
 
+import React from "react";
+import { View, Image, Text, TouchableOpacity } from "react-native";
+import Carousel from "react-native-snap-carousel";
 import styles from "./styles";
-import { ScrollView } from "react-native-web";
+import { ScrollView } from "react-native";
 
+export default function Home({ navigation }) {
+  const pesquisaImages = [
+    require("../../../assets/images/image1.png"),
+    require("../../../assets/images/image2.png"),
+    require("../../../assets/images/image3.png"),
+  ];
 
-export default function Home() {
+  const renderCarouselItem = ({ item, index }) => (
+    <TouchableOpacity onPress={() => handleImagePress(index)}>
+      <View style={styles.blocoPesquisa}>
+        <Image style={styles.imgs} source={item} />
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Text style={styles.Titulo1}> GP Sustentabilidade</Text>
         <Image
           style={styles.imagem1}
-          source={require('../../../assets/gp com fundo de plantas.jpeg')}
+          source={require("../../../assets/images/gp - sustentabilidade.png")}
         />
-
-          <View style={styles.viewPesquisa}>
-            <Text style={styles.titlePesquisa} >Descubra mais sobre as atualidades desse tópico!</Text>
-
-            <View style={styles.blocoPesquisa}>
-              <Image style={styles.imgs} source={require('../../../assets/images/image1.jpeg')} />
-            </View>
-
-            <View style={styles.blocoPesquisa}>
-              <Image style={styles.imgs} source={require('../../../assets/images/image2.jpeg')} />
-            </View>
-            <View style={styles.blocoPesquisa}>
-              <Image style={styles.imgs} source={require('../../../assets/images/image3.jpeg')} />
-            </View>
+        <View style={styles.viewPesquisa}>
+          <Text style={styles.titlePesquisa}>Cheque nossas matérias:</Text>
+          <View style={styles.carousel}>
+            <Carousel
+              data={pesquisaImages}
+              renderItem={renderCarouselItem}
+              sliderWidth={300}
+              itemWidth={300}
+              loop={true}
+              autoplay={true}
+              autoplayInterval={3000}
+              layout="default"
+              style={styles.carousel}
+            />
           </View>
 
+          <Text style={styles.linha}>━━━━━━━━━━━━━━━━━━━</Text>
 
+          <View style={styles.blocoServicos}>
+            <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+              <Image
+                style={styles.imgs1}
+                source={require("../../../assets/images/card3.png")}
+              />
+            </TouchableOpacity>
+          </View>
 
+          <View style={styles.blocoServicos}>
+            <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+              <Image
+                style={styles.imgs1}
+                source={require("../../../assets/images/card2.png")}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.blocoServicos}>
+            <TouchableOpacity onPress={() => navigation.navigate('')}>
+              <Image
+                style={styles.imgs1}
+                source={require("../../../assets/images/card1.png")}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
-
-      
+      </View>
     </ScrollView>
   );
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -2,62 +2,79 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
 
- 
-
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F1EBEB"
+    backgroundColor: "#fff"
   },
-  Titulo1:{
-    fontSize: 20,
-    marginTop: 30,
-    fontWeight: 'bold',
-    fontFamily: 'sans-serif',
-    color: "#A493C2"
-  },
-
+ 
   containerBlocos:{
     margin:20,
     padding:5,
-  
   },
 
   imagem1: {
     width: 430,
-    height: 250,
-    margin: 20,
+    height:250,
+    marginTop: -3,
+    borderRadius: 30,
   },
 
   blocoPesquisa: {
-    marginTop:"20px",
+    marginTop:"15px",
     borderRadius: 5,
-    width:"80vw",
     height:"28vh",
-    marginLeft:"33px",
     padding:5,
-    margin:"20px",
     border: "9px outset #ccb4d9",
     borderRadius:"10px"
-    
+  },
 
+  titlePesquisa: {
+    margin: 20,
+    marginTop: 25,
+    fontWeight: 'bold',
+    color: "#A493C2",
+    fontSize: 18,
+    marginBottom: -5,
   },
 
   viewPesquisa:{
-    marginTop:"50px",
-   
+    marginTop:"10px",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    maxWidth: 400,
+    marginLeft: -5,
+  },
+
+  linha: {
+    color: '#A493C2',
+    marginLeft: 40,
 
   },
 
   imgs:{
-    width: "70vw",
-    height: "21vh",
-    marginLeft:5,
-    marginTop:"10px",
+    width: "65vw",
+    height: "25vh",
     borderRadius:"10px",
   },
-  
+
+  imgs1:{
+    width: "80vw",
+    height: "18vh",
+    gap: 10,
+    marginLeft: 20,
+    margin: 10,
+    borderRadius:"15px",
+  },
+ 
+  carousel:{
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10,
+  }
+ 
 
 });
 
